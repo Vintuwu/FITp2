8727ec581368f9ec1107b0ebbfb404fe7d74c670
Camuss0 <camussovalentin10@gmail.com>
2024-10-06 02:17:28 -0300

    add noticia, deporte, rol periodista

---
+++ Backend/src/api/deporte/routes/deporte.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * deporte router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::deporte.deporte');
---
+++ Backend/src/api/deporte/services/deporte.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * deporte service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::deporte.deporte');
