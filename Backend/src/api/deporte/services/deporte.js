2b2e56348883ce5f49ab7a2aaddb20fe39244d7d
Camuss0 <camussovalentin10@gmail.com>
2024-10-06 21:31:46 -0300

    rollback (este repo es backend)

--- Backend/src/api/deporte/services/deporte.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * deporte service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::deporte.deporte');
---
+++ src/api/deporte/controllers/deporte.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * deporte controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::deporte.deporte');
